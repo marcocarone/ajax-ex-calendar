8263ce79ba9826e5c43552fdf718550817bbf820
Marco <marco.carone@libero.it>
2020-02-03 17:32:57 +0100

    fine js  e inizio layout css

--- js/script.js
+++ js/script.js
@@ -1,20 +1,50 @@
-// Creare un calendario dinamico con le festività. Partiamo dal gennaio 2018 dando la possibilità di cambiare mese, gestendo il caso in cui l’API non possa ritornare festività. Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018 (unici dati disponibili sull’API).
-//
-// Ogni volta che cambio mese dovrò:
-// 1.	Controllare se il mese è valido (per ovviare al problema che l’API non carichi holiday non del 2018)
-// 2.	Controllare quanti giorni ha il mese scelto formando così una lista
-// 3.	Chiedere all’api quali sono le festività per il mese scelto
-// 4.	Evidenziare le festività nella lista
 $(document).ready(function() {
+  moment.locale("it")
+  var mese = 1;
+  chiamataAjax(mese)
 
-  var dataIniziale = moment("2018-01-01", "YYYY-MM-DD");
+
+  $('#successivo').on('click', function() {
+    mese += 1;
+    if (mese > 12) {
+      alert('Il calendario del 2019 non è disponibile!');
+    } else {
+      $('.giorni-mese').text('');
+      chiamataAjax(mese)
+    }
+  });
+
+  $('#precedente').on('click', function() {
+    mese -= 1;
+    if (mese < 1) {
+      alert('Il calendario del 2017 non è disponibile!');
+    } else {
+      $('.giorni-mese').text('');
+      chiamataAjax(mese)
+    }
+  });
+});
+
+
+function AggiungiZero(day) {
+  if (day < 10) {
+    return '0' + day
+  }
+  return day;
+}
+
+
+function chiamataAjax(mese) {
+
+  var dataIniziale = moment("2018-" + mese, "YYYY-MM");
+  console.log("data iniziale :" + dataIniziale);
   // console.log(dataIniziale);
   var meseAnno = dataIniziale.format("MMMM YYYY");
   // console.log(meseAnno);
-  var giorni = dataIniziale.daysInMonth();
-  // console.log(giorni);
-
   $('.mese_anno').text(meseAnno);
+
+  var giorni = dataIniziale.daysInMonth();
+  console.log(giorni);
 
   for (var i = 1; i <= giorni; i++) {
     var giorno = AggiungiZero(i);
@@ -29,32 +59,14 @@ $(document).ready(function() {
     $(".giorni-mese").append(html);
   }
 
-  chiamataAjax()
+  var url = 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=' + (mese - 1);
 
-});
-
-
-function AggiungiZero(day) {
-  if (day < 10) {
-    return '0' + day
-  }
-  return day;
-}
-
-
-function chiamataAjax() {
   $.ajax({
-    url: 'https://flynn.boolean.careers/exercises/api/holidays',
+    url: url,
     method: 'GET',
-    data: {
-      year: 2018,
-      month: 0
-    },
     success: function(data) {
-
       var festivita = data.response;
       console.log(festivita);
-
       $('.giorni-mese li').each(function() {
         var data_giornoMese = $(this).attr('data-giorno');
         console.log("giorno mese " + data_giornoMese);
