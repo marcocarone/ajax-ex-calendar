27e04d0a497e47c638376b4c4091e6436d00d4ff
Marco <marco.carone@libero.it>
2020-02-03 14:58:05 +0100

    prima fase per mese gennaio

--- js/script.js
+++ js/script.js
@@ -5,3 +5,70 @@
 // 2.	Controllare quanti giorni ha il mese scelto formando così una lista
 // 3.	Chiedere all’api quali sono le festività per il mese scelto
 // 4.	Evidenziare le festività nella lista
+$(document).ready(function() {
+
+  var dataIniziale = moment("2018-01-01", "YYYY-MM-DD");
+  // console.log(dataIniziale);
+  var meseAnno = dataIniziale.format("MMMM YYYY");
+  // console.log(meseAnno);
+  var giorni = dataIniziale.daysInMonth();
+  // console.log(giorni);
+
+  $('.mese_anno').text(meseAnno);
+
+  for (var i = 1; i <= giorni; i++) {
+    var giorno = AggiungiZero(i);
+    // console.log(giorno);
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    var context = {
+      "giorno": giorno,
+      "numeroGiorno": dataIniziale.format('YYYY-MM-') + AggiungiZero(i),
+    };
+    var html = template(context);
+    $(".giorni-mese").append(html);
+  }
+
+  chiamataAjax()
+
+});
+
+
+function AggiungiZero(day) {
+  if (day < 10) {
+    return '0' + day
+  }
+  return day;
+}
+
+
+function chiamataAjax() {
+  $.ajax({
+    url: 'https://flynn.boolean.careers/exercises/api/holidays',
+    method: 'GET',
+    data: {
+      year: 2018,
+      month: 0
+    },
+    success: function(data) {
+
+      var festivita = data.response;
+      console.log(festivita);
+
+      $('.giorni-mese li').each(function() {
+        var data_giornoMese = $(this).attr('data-giorno');
+        console.log("giorno mese " + data_giornoMese);
+
+        for (var i = 0; i < festivita.length; i++) {
+          if (festivita[i].date == data_giornoMese) {
+            $(this).addClass('red');
+            $(this).append(" " + festivita[i].name);
+          }
+        }
+      });
+    },
+    error: function(richiesta, stato, errori) {
+      console.log(errori);
+    }
+  });
+}
