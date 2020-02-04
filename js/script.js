f7a8e9893929f7402cf3d35cc55378298f6729b2
Marco <marco.carone@libero.it>
2020-02-04 10:06:47 +0100

    AGGIUNTI COMMENTI

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
+  //traduco momentjs in italiano
   moment.locale("it")
   var mese = 1;
   chiamataAjax(mese)
 
-
+  // click su mese successivo
   $('#successivo').on('click', function() {
     mese += 1;
     if (mese > 12) {
@@ -13,7 +14,7 @@ $(document).ready(function() {
       chiamataAjax(mese)
     }
   });
-
+  // click su mese precedente
   $('#precedente').on('click', function() {
     mese -= 1;
     if (mese < 1) {
@@ -25,7 +26,7 @@ $(document).ready(function() {
   });
 });
 
-
+// FUNZIONI DELLO SCRIPT
 function AggiungiZero(day) {
   if (day < 10) {
     return '0' + day
@@ -33,22 +34,17 @@ function AggiungiZero(day) {
   return day;
 }
 
-
 function chiamataAjax(mese) {
-
+  // stabilisco la data iniziale nel formato anno-mese
   var dataIniziale = moment("2018-" + AggiungiZero(mese), "YYYY-MM");
-  console.log("data iniziale :" + dataIniziale);
-  // console.log(dataIniziale);
+  //creo una variabile che contiene il mese e l'anno da inserire nel container__top
   var meseAnno = dataIniziale.format("MMMM YYYY");
-  // console.log(meseAnno);
   $('.mese_anno').text(meseAnno);
-
+  // creo la var giorni che mi servirà, tramite un ciclo for per stabilire i giorni di ogni mese
   var giorni = dataIniziale.daysInMonth();
-  console.log(giorni);
-
+  // con il ciclo for e l'utilizzo di handlebars vado a popolare il container con i giorni esatti di ogni mese
   for (var i = 1; i <= giorni; i++) {
     var giorno = AggiungiZero(i);
-    // console.log(giorno);
     var source = $("#entry-template").html();
     var template = Handlebars.compile(source);
     var context = {
@@ -58,7 +54,7 @@ function chiamataAjax(mese) {
     var html = template(context);
     $(".giorni-mese").append(html);
   }
-
+  // creo la var del link per la chiamata API - per ricercare il mese esatto in automatico aggiungo al link la var mese - 1, in quanto l'anno, cioè gennaio parte dal numero 0
   var url = 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=' + (mese - 1);
 
   $.ajax({
@@ -66,13 +62,12 @@ function chiamataAjax(mese) {
     method: 'GET',
     success: function(data) {
       var festivita = data.response;
-      console.log(festivita);
+      // faccio un ciclo per ogni elemento - giorno del mese- utilizzando each
       $('.caselle').each(function() {
-        var data_giornoMese = $(this).attr('data-giorno');
-        console.log("giorno mese " + data_giornoMese);
-
+        var dataGiornoMese = $(this).attr('data-giorno');
+        //con un ciclo for vado a vedere se la data indicata negli oggetti dell API è uguale a quella presente nei data-giorno del mese, se si aggiungo una classe per cambiare colore e infine aggiungo il nome della festività
         for (var i = 0; i < festivita.length; i++) {
-          if (festivita[i].date == data_giornoMese) {
+          if (festivita[i].date == dataGiornoMese) {
             $(this).addClass('caselle__festivita');
             $(this).children().append(" " + festivita[i].name);
           }
